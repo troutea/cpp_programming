56346b63e0f32869859092dc03d112935219a595
troutea <troutea@yahoo.co.uk>
2024-06-05 15:39:11 +0100

    adding updated files

---
+++ exercise_classes/Dog.h
@@ -0,0 +1,25 @@
+#ifndef _DOG_H_
+#define  _DOG_H_
+
+#include <string>
+
+class Dog {
+
+private:
+std::string name;
+int age;
+
+public:
+
+std::string  get_name();
+int get_age(); 
+void set_name(std::string name);
+void set_age(int age);
+
+
+protected:
+
+
+};
+
+#endif
---
+++ exercise_classes/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+#include "exercise_classes/Dog.h"
+
+
+using namespace std;
+
+
+int main() {
+
+     Dog dog;
+
+
+
+}
