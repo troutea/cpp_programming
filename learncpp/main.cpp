315f14126c771f28370c65a51adb3308ba84ab5f
troutea <troutea@yahoo.co.uk>
2025-09-11 09:25:16 +0100

    updating files

--- learncpp/main.cpp
+++ learncpp/main.cpp
@@ -4,5 +4,15 @@
 int main()
 {
 
-    std::cout << "Hello World" << '\n';
+int x {5};
+int y {5};
+std::cout << x << ' ' << y << '\n';
+std::cout << ++x << ' ' << --y << '\n';
+std::cout << x << ' ' << y << '\n';
+std::cout << x++ << ' ' << y-- << '\n';
+std::cout << x << ' ' << y << '\n';
+
+return 0;
+
 }
+
